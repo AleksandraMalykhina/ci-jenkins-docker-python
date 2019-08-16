eedbf9bd79925ea7d064846367ca62866bdd790c
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-08-16 13:46:36 +0300

    test

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,8 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(Subtract(8, 6), 2)
+	def test_subtract(self):
+        self.assertEqual(Subtract(8, 6), 2)
 
 if __name__ == '__main__':
     unittest.main()
